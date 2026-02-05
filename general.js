17d7131c2eafedbb65264767a02da77fa234b0e5
Maxwell <omokaromaxwell90@gmail.com>
2026-02-05 14:33:38 +0000

    Create folder structure

--- general.js
+++
@@ -1,7 +0,0 @@
-function option_no() {
-    alert("Unfortunately you can't click no, na by fire by force.");
-}
-
-function option_yes() {
-    alert("Thank you for choosing me to be your Valentine, you mean so much to me. Other surprises coming don't worry.");
-}
